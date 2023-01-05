f23f158584df2faa7fdf53d7165814730ff20571
elotech47 <eloghosaefficiency@gmail.com>
2023-01-05 12:49:48 -0600

    main deephive algorithm file

---
+++ algorithms/deephive/deephive.py
@@ -0,0 +1,73 @@
+"""
+DeepHive Algorithm Implementation: A multi-agent reinforcement learning algorithm for black-box optimization.
+
+"""
+#imports
+import numpy as np
+import random
+import math
+from typing import Callable, List, Tuple
+from environment import OptEnv, OptEnvCache
+from mappo import MAPPO
+import os
+from argparse import ArgumentParser
+import yaml
+from optimization_functions import *
+
+def get_args():
+    """
+    Get the command line arguments.
+    :return: The command line arguments.
+    """
+    # create the parser
+    parser = ArgumentParser()
+    # add the arguments
+    parser.add_argument('-c', '--config', type=str, default='config.yaml', help='The path to the config file.')
+    # parse the arguments
+    args = parser.parse_args()
+    # return the arguments
+    return args
+
+def get_config(path:str) -> dict:
+    """
+    Get the config from the yaml file.
+    :param path: The path to the yaml file.
+    :return: The config.
+    """
+    # check if the file exists
+    if not os.path.exists(path):
+        raise FileNotFoundError('The config file does not exist.')
+    # open the file
+    with open(path, 'r') as file:
+        # load the yaml file
+        config = yaml.safe_load(file)
+    # return the config
+    return config
+
+def prepare_environment(config:dict) -> OptEnvCache:
+    """
+    Prepare the environment.
+    :param config: The config.
+    :return: The environment.
+    """
+    # prepare each environment as specified in the config file and cache them
+    env_config = config['environment_config']
+    for i in range(len(env_config['env_list'])):
+        # create the environment
+        func_name = env_config['env_list'][i] + "_" + f"{env_config['n_dim']}D_{env_config['n_agents']}_agents"
+        obj_func, bounds = get_obj_func(env_config['env_list'][i])
+        # if n_dim is not 2D, ignore the bounds and use bounds from the config file
+        if env_config['n_dim'] != 2:
+            bounds = env_config['bounds'][i]
+        env = OptEnv(func_name, obj_func, env_config['n_agents'], env_config['n_dim'], 
+                    bounds, env_config['ep_length'], env_config['minimize'], env_config['freeze']
+                    )
+        # cache the environment
+        OptEnvCache.add_env(env)
+    # return the environment
+    return OptEnvCache
+
+def main():
+    pass
+
+    
